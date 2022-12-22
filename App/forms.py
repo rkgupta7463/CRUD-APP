832ac8871a032433ce693493a7e3ce625caa997a
unknown <rishukumargupta8409@gmail.com>
2022-12-22 20:07:01 +0530

    First commit

--- App/forms.py
+++ App/forms.py
@@ -7,3 +7,4 @@ class StudentInfoForm(forms.ModelForm):
         fields=['sname','semail','phone','Branch','college']
         labels={'sname':'Name','semail':'Email', 'phone':'Phone','Branch':'Branch','college':'College'}
         widgets={'sname':forms.TextInput(attrs={'class':'form-control'}), 'semail':forms.EmailInput(attrs={'class':'form-control'}), 'phone':forms.NumberInput(attrs={'class':'form-control'}), 'Branch':forms.TextInput(attrs={'class':'form-control'}),'college':forms.TextInput(attrs={'class':'form-control'})}
+    
